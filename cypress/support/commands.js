a3fd6ce515bd36806fa2568afbb1608e003e7013
Bruno Oliveira <bruno.oliveira@me.com.br>
2022-06-04 22:02:38 -0300

    Atualização do projeto

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,44 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('setToken', function(){
+    cy.api({
+        method: 'POST',
+        url: '/sessions',
+        body: {
+            email: 'bruno@qacademy.io',
+            password: 'qa-cademy'
+        },
+        failOnStatusCode: false
+    }).then(function(response){
+        expect(response.status).to.eql(200)
+        Cypress.env('token', response.body.token)
+    })
+})
+
+Cypress.Commands.add('back2ThePast', function(){
+    cy.api({
+        method: 'DELETE',
+        url: '/back2thepast/6293b200335e8e001620a596',
+        failOnStatusCode: false
+    }).then(function(response){
+        expect(response.status).to.eql(200)
+    })
+})
+
+
+//POST requisição que testa o cadastro de um personagem
+Cypress.Commands.add('postCharacter', function(payload){
+    cy.api({
+        method: 'POST',
+        url: '/characters',
+        body: payload,
+        headers: {
+            Authorization: Cypress.env('token')
+        },
+        failOnStatusCode: false
+    }).then(function(response){
+        return response
+    })
+})
